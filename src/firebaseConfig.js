f68c571cdc77581ee1003f85ddf3c22cdfd67b27
automacaofatuaras <automacaofaturas@gmail.com>
2026-01-16 16:40:18 -0300

    deploy 2

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 
 // Substitui estes valores pelos que copiaste do Console do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSy...", 
+  apiKey: "AIzaSyDEH_KYVt62F0KEuVh8qgZqu0SmjP7UKK8",
   authDomain: "portal-scamatti.firebaseapp.com",
   projectId: "portal-scamatti",
-  storageBucket: "portal-scamatti.appspot.com",
-  messagingSenderId: "123...",
-  appId: "1:123..."
+  storageBucket: "portal-scamatti.firebasestorage.app",
+  messagingSenderId: "54471460240",
+  appId: "1:54471460240:web:c4be92b193013dd7525f15"
 };
 
 const app = initializeApp(firebaseConfig);
